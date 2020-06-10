0546151c33236a733955bd171c9121f1ee9a68d6
dennis <dennis.olimpio0794@gmail.com>
2020-06-10 20:09:00 +0200

    JS Add doppio click per mostrare/ nascondere dropdown

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
-$(document).ready( function()
+$(document).ready(
 
-{
+  function(){
 
 // DEBUG
 $(".with-dropdown").mouseenter(
@@ -11,10 +11,29 @@ $(this).children(".dropdown").addClass("active");
 
 });
 
-$(".with-dropdown").mouseleave( function () {
+$(".with-dropdown").mouseleave(
+
+  function () {
   $(this).children(".dropdown").removeClass("active");
 
 });
+
+
+// Con un doppio click chiudo il menu se è aperto, altrimenti lo chiudo.
+
+$(".with-dropdown").dblclick(
+
+  function(){
+
+  if( $(".dropdown").hasClass("active") ) {
+    $(this).children(".dropdown").removeClass("active");
+
+  } else {
+    $(this).children(".dropdown").addClass("active");
+
+  }
+})
+
 
 
 });
